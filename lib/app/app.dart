3882d2d63cc252e43712a529b55259ae1ee57be7
Asus <samandarahadjonovss@gmail.com>
2023-03-02 07:47:24 +0500

    dfsdf

---
+++ lib/app/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyApp();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
---
+++ lib/utils/constants/routes.dart
@@ -0,0 +1,6 @@
+String splash = '/splash';
+String addCard = '/addCard';
+String updateCard = '/updateCard';
+String home = '/home';
+String cards = '/cards';
+String payment = '/payment';

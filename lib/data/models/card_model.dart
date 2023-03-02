0192b5fde47a4d05f2f86648cb350c11380a4584
Asus <samandarahadjonovss@gmail.com>
2023-03-02 08:27:16 +0500

    dfsdf

---
+++ lib/data/models/card_model.dart
@@ -0,0 +1,52 @@
+class CardModel{
+  final String expireDate;
+  final String cardNumber;
+  final String image;
+  final List<String> gradient;
+
+  CardModel({
+    required this.image,
+    required this.cardNumber,
+    required this.expireDate,
+     this.gradient = const ["",""]
+});
+
+  factory CardModel.fromJson(Map<String,dynamic> json){
+    return CardModel(
+      image: json[CardModeFields.image],
+      cardNumber: json[CardModeFields.cardNumber],
+      expireDate: json[CardModeFields.expireDate],
+      gradient: [json[CardModeFields.firstColor],json[CardModeFields.secondColor]]
+    );
+  }
+
+  toJson(){
+    return {
+      CardModeFields.expireDate:expireDate,
+      CardModeFields.cardNumber:cardNumber,
+      CardModeFields.image:image,
+      CardModeFields.firstColor:gradient[0],
+      CardModeFields.secondColor:gradient[1],
+    };
+  }
+
+
+
+}
+
+
+
+class CardModeFields{
+  static const expireDate= "expireDate";
+  static const cardNumber= "cardNumber";
+  static const image= "image";
+  static const firstColor= "firstColor";
+  static const secondColor= "secondColor";
+}
+
+
+
+
+
+
+
